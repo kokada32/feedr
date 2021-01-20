c498a812e33ec269a732a13c843602a763951d72
Kenny Okada <kokada32@gmail.com>
2021-01-19 22:00:20 -0800

    added 2 cities to news api and weather api

--- index.js
+++ index.js
@@ -1,154 +1,160 @@
 
 var url = "http://newsapi.org";
 var apiKey = "d2b569b91d5940f4948a00876790367f";
+var apiData = [];
+var urlWeather = "https://www.metaweather.com/api/location";
+var apiWeatherData = [];
 
-$("#sourceName").text("News API"); 
-$("#firstSource").text("News API");
-$("#secondSource").text("NPR News");
+$("#sourceName").text("Los Angeles"); 
+$("#firstSource").text("Los Angeles");
+$("#secondSource").text("New York");
 
-function displayNewsAPI (results) {
-
-  var firstArticle = results.articles[0];
+function displayNewsAPI () {
+  var firstArticle = apiData.articles[0];
   $("#article1").text(firstArticle.title);
   $("#article1Img").attr("src", firstArticle.urlToImage);
   $("#description1").text(firstArticle.description);
 
-  var secondArticle = results.articles[1];
+  var secondArticle = apiData.articles[1];
   $("#article2").text(secondArticle.title);
   $("#article2Img").attr("src", secondArticle.urlToImage);
   $("#description2").text(secondArticle.description);
 
-  var thirdArticle = results.articles[2];
+  var thirdArticle = apiData.articles[2];
   $("#article3").text(thirdArticle.title);
   $("#article3Img").attr("src", thirdArticle.urlToImage);
   $("#description3").text(thirdArticle.description);
 
-  var fourthArticle = results.articles[3];
+  var fourthArticle = apiData.articles[3];
   $("#article4").text(fourthArticle.title);
   $("#article4Img").attr("src", fourthArticle.urlToImage);
   $("#description4").text(fourthArticle.description);
 }
 
-function displayPopUp1 (results) {
-  var firstArticle = results.articles[0];
+function displayPopUp1 () {
+  var firstArticle = apiData.articles[0];
   $("#popUp").toggleClass();
   $("#popUpTitle").text(firstArticle.title);
   $("#popUpContent").text(firstArticle.content);
   $("#popUpURL").attr("href", firstArticle.url);
 }
 
-function displayPopUp2 (results) {
-  var firstArticle = results.articles[1];
+function displayPopUp2 () {
+  var firstArticle = apiData.articles[1];
   $("#popUp").toggleClass();
   $("#popUpTitle").text(firstArticle.title);
   $("#popUpContent").text(firstArticle.content);
   $("#popUpURL").attr("href", firstArticle.url);
 }
 
-function displayPopUp3 (results) {
-  var firstArticle = results.articles[2];
+function displayPopUp3 () {
+  var firstArticle = apiData.articles[2];
   $("#popUp").toggleClass();
   $("#popUpTitle").text(firstArticle.title);
   $("#popUpContent").text(firstArticle.content);
   $("#popUpURL").attr("href", firstArticle.url);
 }
 
-function displayPopUp4 (results) {
-  var firstArticle = results.articles[3];
+function displayPopUp4 () {
+  var firstArticle = apiData.articles[3];
   $("#popUp").toggleClass();
   $("#popUpTitle").text(firstArticle.title);
   $("#popUpContent").text(firstArticle.content);
   $("#popUpURL").attr("href", firstArticle.url);
+}
+
+function displayWeather () {
+  var weather = apiWeatherData.consolidated_weather[0];
+  $("#weatherTitle").text(apiWeatherData.title);
+  $("#weatherTemp").text(Math.round((weather.the_temp * 9/5) + 32) + " degrees");
+  $("#weatherState").text(weather.weather_state_name);
 }
 
 $.get(
-  `https://accesscontrolalloworiginall.herokuapp.com/${url}/v2/top-headlines?country=us&apiKey=${apiKey}`,
+  `https://accesscontrolalloworiginall.herokuapp.com/${url}/v2/everything?q=Los+Angeles&apiKey=${apiKey}`,
   function (results) {
-    console.log(results);
-    displayNewsAPI(results);
+    apiData = results;
+    pageSource = "LA";
+    console.log(apiData);
+    displayNewsAPI();
   }
 );
 
-$("#article1").on("click", function () {
-  $.get(
-    `https://accesscontrolalloworiginall.herokuapp.com/${url}/v2/top-headlines?country=us&apiKey=${apiKey}`,
-    function (results) {
-      displayPopUp1(results);
-    }
-  );
+$.get (
+  `https://accesscontrolalloworiginall.herokuapp.com/${urlWeather}/2442047`,
+  function (results) {
+    apiWeatherData = results;
+    console.log(results);
+    displayWeather();
+  }
+);
+
+$("#article1").on("click", () => {
+  displayPopUp1();
 });
 
-$("#article2").on("click", function () {
-  $.get(
-    `https://accesscontrolalloworiginall.herokuapp.com/${url}/v2/top-headlines?country=us&apiKey=${apiKey}`,
-    function (results) {
-      displayPopUp2(results);
-    }
-  );
+$("#article2").on("click", () => {
+  displayPopUp2();
 });
 
-$("#article3").on("click", function () {
-  $.get(
-    `https://accesscontrolalloworiginall.herokuapp.com/${url}/v2/top-headlines?country=us&apiKey=${apiKey}`,
-    function (results) {
-      displayPopUp3(results);
-    }
-  );
+$("#article3").on("click", () => {
+  displayPopUp3();
 });
 
-$("#article4").on("click", function () {
-  $.get(
-    `https://accesscontrolalloworiginall.herokuapp.com/${url}/v2/top-headlines?country=us&apiKey=${apiKey}`,
-    function (results) {
-      displayPopUp4(results);
-    }
-  );
+$("#article4").on("click", () => {
+  displayPopUp4();
 });
 
 $("#closePopUp").on("click", function () {
     $("#popUp").attr("class", "loader hidden");
 });
 
-$("#firstSource").on("click", function () {
-  $.get(
-    `https://accesscontrolalloworiginall.herokuapp.com/${url}/v2/top-headlines?country=us&apiKey=${apiKey}`,
+$("#firstSource").on("click", () => {
+  if (pageSource === 'LA') {
+    console.log('already on LA source');
+  } else {
+    $.get(
+    `https://accesscontrolalloworiginall.herokuapp.com/${url}/v2/everything?q=Los+Angeles&apiKey=${apiKey}`,
     function (results) {
-      displayNewsAPI(results);
-    }
-  );
-});
+      apiData = results;
+      pageSource = "LA"
+      displayNewsAPI(apiData);
+      }
+    );
 
-var urlNPR = "https://authorization.api.npr.org";
+    $.get (
+      `https://accesscontrolalloworiginall.herokuapp.com/${urlWeather}/2442047`,
+      function (results) {
+        apiWeatherData = results;
+        displayWeather();
+      }
+    );
+  }
+});
 
 $("#secondSource").on("click", function () {
-  $.post(
-    `https://accesscontrolalloworiginall.herokuapp.com/${urlNPR}/v2/token`,
-    {
-      grant_type: "authorization_code",
-      client_id: "nprone_trial_z6cZazWSSCxs",
-      client_secret: "P6WY5vaFTz3mLMZhM7Ixyy1HkjG9ibptGYSPa4AB"
-    },
-    function (results) {
-      console.log(results);
-    }
-  );
+  if (pageSource === 'NY'){
+    console.log('already on NY source');
+  } else {
+    $.get(
+      `https://accesscontrolalloworiginall.herokuapp.com/${url}/v2/everything?q=New+York&apiKey=${apiKey}`,
+      function (results) {
+        apiData = results;
+        pageSource = "NY";
+        displayNewsAPI(apiData);
+      }
+    );
+
+    $.get (
+      `https://accesscontrolalloworiginall.herokuapp.com/${urlWeather}/2459115`,
+      function (results) {
+        apiWeatherData = results;
+        displayWeather();
+      }
+    );
+  }
 });
 
-// $("#secondSource").on("click", function() {
 
 
-// const settings = {
-// 	"async": true,
-// 	"crossDomain": true,
-// 	"url": "https://community-hacker-news-v1.p.rapidapi.com/topstories.json?print=pretty",
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-key": "0d079c05a5mshdacb83be5656a0cp1e3bb7jsna972f414725c",
-// 		"x-rapidapi-host": "community-hacker-news-v1.p.rapidapi.com"
-// 	}
-// };
 
-// $.ajax(settings).done(function (response) {
-// 	console.log(response);
-// });
-// });
